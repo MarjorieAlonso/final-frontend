0fdd95f4eac3c0ce4e5f1ce3331dcdb18fcd25ac
Melissa Guerra <melissaguerrachamorro@gmail.com>
2024-09-13 17:55:00 -0500

    chance detail

--- src/Routes/Detail.jsx
+++ src/Routes/Detail.jsx
@@ -1,24 +1,55 @@
-//import React from 'react'
-import {useContextGlobal} from "../Components/utils/Context"
+import { useParams } from 'react-router-dom';
+import {useState, useEffect} from 'react';
+import axios from 'axios';
 
 
 //Este componente debera ser estilado como "dark" o "light" dependiendo del theme del Context
 
 const Detail = () => {
- const{detail} = useContextGlobal()
-
+  const [show,setShow] = useState(true)
+  const params=useParams()
+  const [dentist,setDentist]=useState()
  
+  
   // Consumiendo el parametro dinamico de la URL deberan hacer un fetch a un user en especifico
+   const url=`https://jsonplaceholder.typicode.com/users/${params.id}`
+
+useEffect(() =>{
+  axios(url)
+  .then((res) =>{
+    setDentist(res.data)
+    setTimeout(()=>{
+      setShow(false)  
+    },1000)
+  })
+},[url]);
 
   return (
     <>
-      <h1>Detalle de dentistas</h1>
-      <h2>{detail.name}</h2>
-      <p>{detail.email}</p>
-      <p>{detail.phone}</p>
-      <p>{detail.website}</p>
-      {/* aqui deberan renderizar la informacion en detalle de un user en especifico */}
-      {/* Deberan mostrar el name - email - phone - website por cada user en especifico */}
+      {show ? ("Loading...")
+    : (
+      <div>
+        <h1>Detail Dentist {dentist.id} </h1>
+        <div className="detailCard">
+          <div>
+            <h4>Nombre</h4>
+            <p>{dentist.name}</p>
+          </div>
+          <div>
+            <h4>Email</h4>
+            <p>{dentist.email}</p>
+          </div>
+          <div>
+            <h4>Phone</h4>
+            <p>{dentist.phone}</p>
+          </div>
+          <div>
+            <h4>Website</h4>
+            <p>{dentist.website}</p>
+          </div>
+        </div>
+      </div>
+    )}
     </>
   )
 }
